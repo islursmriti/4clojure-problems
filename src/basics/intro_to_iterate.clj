4560119bc7c132b70473c5776228ebdc7ad0a50d
islursmriti <smritivenkatislur@gmail.com>
2025-02-06 11:38:10 +0530

    Add code and test files for some and iterate function concepts

---
+++ src/basics/intro_to_iterate.clj
@@ -0,0 +1,8 @@
+(ns basics.intro-to-iterate)
+
+
+(defn problem-45
+  "demonstrates functionality of iterate function
+   ref: https://4clojure.oxal.org/#/problem/45"
+  [value]
+  (= value (take 5 (iterate #(+ 3 %) 1))))
